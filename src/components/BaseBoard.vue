7487b042d89666210bfc739fe9d53322d0658ea5
ShawnL <45999699+shawnlin0201@users.noreply.github.com>
2023-04-11 09:58:54 +0800

    feat: 將 board 改為二維陣列運算

--- src/components/BaseBoard.vue
+++ src/components/BaseBoard.vue
@@ -32,20 +32,14 @@
     <div class="relative z-2 left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 w-90 h-90 bg-yellow">
       <div class="absolute z-1 grid grid-rows-9 grid-cols-9">
         <template v-for="row in 9">
-          <div
-            @click="board.putChess(board.getCurrPlayerColor(), `${numberToChar(col)}${numberToChar(row)}`)"
-            class="w-10 h-10 flex justify-center items-center"
-            v-for="col in 9"
-            :key="row + col"
-          >
+          <div class="w-10 h-10 flex justify-center items-center" v-for="col in 9" :key="row + col">
             <BaseStone
-              :class="
-                board.getPointColor({ x: numberToChar(col), y: numberToChar(row) }) === 'B' ? 'bg-black' : 'bg-white'
-              "
-              v-if="board.isOccupied({ x: numberToChar(col), y: numberToChar(row) })"
+              :class="board.getPointColor({ x: col - 1, y: row - 1 }) === 'B' ? 'bg-black' : 'bg-white'"
+              v-if="board.isOccupied({ x: col - 1, y: row - 1 })"
             ></BaseStone>
             <BaseStone
               v-else
+              @click="board.putChess({ color: board.getCurrPlayerColor(), x: col - 1, y: row - 1 })"
               :class="
                 board.getCurrPlayerColor() === 'B'
                   ? 'hover:border-2 hover:border-black'
@@ -62,14 +56,8 @@
 <script setup>
 import { ref } from 'vue'
 import { minigo } from '@/utils/chess'
-const numberToChar = (num) => {
-  return String(num).replace(/[1-9]/g, (match) => {
-    const code = match.charCodeAt(0)
-    return String.fromCharCode(code + 96 - 48)
-  })
-}
-const sgfInfo = ref(`(;FF[4]GM[1]SZ[9];B[cd];W[dd];B[de];W[ef];B[dc])`)
-// const sgfInfo = ref(`(;GM[1]SZ[9];B[cd];W[dd];B[de];W[ef];B[dc];W[ff])`)
+// const sgfInfo = ref(`(;FF[4]GM[1]SZ[9];B[cd];W[dd];B[de];W[ef];B[dc])`)
+const sgfInfo = ref(`(;GM[1]SZ[9];B[aa];W[bb];B[cd];W[dd];B[de];W[ef];B[dc];W[ff])`)
 const board = new minigo.Board(sgfInfo.value)
 const XAxis = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']
 </script>
--- src/utils/chess.js
+++ src/utils/chess.js
@@ -1,26 +1,42 @@
-import { ref } from 'vue'
-
+import { ref, reactive } from 'vue'
+const charToNumber = (str) => {
+  const arrOffset = 1
+  return str.replace(/[a-zA-Z]/g, (match) => {
+    const code = match.toLowerCase().charCodeAt(0)
+    if (code >= 97 && code <= 122) {
+      return code - 96 - arrOffset
+    }
+    return match
+  })
+}
+const numberToChar = (num) => {
+  const arrOffset = 1
+  return String(num).replace(/[0-8]/g, (match) => {
+    const code = match.charCodeAt(0)
+    return String.fromCharCode(code + 96 - 48 + arrOffset)
+  })
+}
 export const minigo = (() => {
   class Board {
     constructor(sgf = '') {
       this.gameInfo = ref({
         FF: '',
       })
-      this.moves = ref([])
-      this.chessOnBoard = ref([])
+      this.historyMoves = reactive([])
+      this.board = reactive(new Array(9).fill().map(() => new Array(9).fill(null)))
       this.sgf = ref(sgf)
       this.currPlayerColor = ref('')
       this.parseSGF(sgf)
     }
     getCurrPlayerColor() {
-      this.currPlayerColor.value = this.moves.value.length % 2 === 0 ? 'B' : 'W'
+      this.currPlayerColor.value = this.historyMoves.length % 2 === 0 ? 'B' : 'W'
       return this.currPlayerColor.value
     }
-    getPointColor(point) {
-      return this.chessOnBoard.value.filter((info) => info.move === `${point.x}${point.y}`)[0].color
+    getPointColor({ x, y }) {
+      return this.board[x][y].color
     }
-    isOccupied(point) {
-      return this.chessOnBoard.value.filter((info) => info.move === `${point.x}${point.y}`).length
+    isOccupied({ x, y }) {
+      return this.board[x][y] !== null
     }
     createNewBoard() {
       this.sgf.value = '(;SZ[9])'
@@ -36,7 +52,7 @@ export const minigo = (() => {
       const moves = []
       let match
       // Extract game info
-      const gameInfoRegex = /(FF|GM|SZ)\[(\d+)\]/g
+      const gameInfoRegex = /(GM|SZ)\[(\d+)\]/g
       while ((match = gameInfoRegex.exec(sgf))) {
         this.gameInfo.value[match[1]] = match[2]
       }
@@ -45,30 +61,33 @@ export const minigo = (() => {
       while ((match = moveRegex.exec(sgf))) {
         moves.push({
           color: match[1],
-          move: match[2],
+          x: charToNumber(match[2][0]),
+          y: charToNumber(match[2][1]),
         })
       }
-      moves.forEach((move) => this.putChess(move.color, move.move))
-      console.log('this.gameInfo', this.gameInfo)
+      moves.forEach((move) => this.putChess({ color: move.color, x: move.x, y: move.y, updateSGF: false }))
       return sgf
     }
-    putChess(color, move) {
+    putChess({ color, x, y, updateSGF = true }) {
       // todo: 驗證合法
       // todo: 吃子判斷
-      this.chessOnBoard.value.push({
+      this.board[x][y] = {
         color,
-        move,
+      }
+
+      this.historyMoves.push({
+        color,
+        x,
+        y,
       })
 
-      this.moves.value.push({
-        color,
-        move,
-      })
-
-      this.updateSGF(color, move)
+      if (updateSGF) {
+        this.updateSGF({ color, x, y })
+      }
     }
-    updateSGF(color, move) {
-      this.sgf.value = this.sgf.value.split(')').join(`;${color}[${move}]`)
+    updateSGF({ color, x, y }) {
+      const newMove = `;${color}[${numberToChar(x)}${numberToChar(y)}]`
+      this.sgf.value = this.sgf.value.replace(')', `${newMove})`)
     }
     undo() {
       // todo: SGF 移除最後一動
